63da8d1d176c3e6cbde5a8e75e9e6d14e54f419c
Abel O'Rian <abel.orian@gmail.com>
2023-10-06 15:43:44 -0300

    Add: basic cron server

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const CronJob = require('cron').CronJob;
+
+
+const app = express()
+app.set("port", process.env.PORT || 3005)
+
+app.get("/", (_, res) => {
+    res.send("Welcome to CRON server!!");
+});
+
+const job = new CronJob(
+	'*/5 * * * * *',
+	function () {
+		console.log(`You will see this message every second ${new Date()}`);
+	},
+	null,
+	true,
+	'America/Los_Angeles'
+);
+
+app.listen(app.get("port"), () => {
+    console.log("Express server listening on port " + app.get("port"));
+});
